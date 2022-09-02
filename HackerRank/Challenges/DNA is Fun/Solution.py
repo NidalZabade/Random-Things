cea23c45b55f0909ace40a7bf8b9e937c8fd834b
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2022-09-02 14:55:13 +0300

    Update Solution.py

--- HackerRank/Challenges/DNA is Fun/Solution.py
+++ HackerRank/Challenges/DNA is Fun/Solution.py
@@ -1,12 +1,5 @@
-test=int(input())
-for i in range(test):
-    N,M,g,y=input().split(" ")
-    total=int(N)*int(M)
-    mod=total-int(g)
-    if mod>=0:
-        if int(y)==0:
-            print(-1)
-        else:
-            print(mod)
-    else:
-        print(0)
+T=int(input())
+for i in range(T):
+    n=int(input())
+    string=input()
+    print(string.count('A')*string.count('G')*string.count('C')*string.count('T'))
