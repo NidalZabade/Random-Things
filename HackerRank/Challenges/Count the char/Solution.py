8555c888980fdbdb997181b8be724604ae76ebe1
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2022-09-02 15:00:25 +0300

    Update Solution.py

--- HackerRank/Challenges/Count the char/Solution.py
+++ HackerRank/Challenges/Count the char/Solution.py
@@ -1,12 +1,12 @@
-test=int(input())
-for i in range(test):
-    N,M,g,y=input().split(" ")
-    total=int(N)*int(M)
-    mod=total-int(g)
-    if mod>=0:
-        if int(y)==0:
-            print(-1)
-        else:
-            print(mod)
-    else:
-        print(0)
+d={}
+number = input()
+string=input()
+string1="abcdefghijklmnopqrstuvwxyz"
+for i in string1:
+    d[i]=0
+for i in string:
+    if i in d:
+        d[i]+=1
+
+for i in sorted(d):
+    print(f"{i}: {d[i]}")
