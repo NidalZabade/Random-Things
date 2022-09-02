4033cb2930f0e42cbf2572340c007176fef621ba
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2022-09-02 14:56:18 +0300

    Update Solution.py

--- HackerRank/Challenges/Count common divisors/Solution.py
+++ HackerRank/Challenges/Count common divisors/Solution.py
@@ -1,12 +1,28 @@
-test=int(input())
-for i in range(test):
-    N,M,g,y=input().split(" ")
-    total=int(N)*int(M)
-    mod=total-int(g)
-    if mod>=0:
-        if int(y)==0:
-            print(-1)
-        else:
-            print(mod)
-    else:
-        print(0)
+from math import gcd as __gcd
+def getCount(a, n):
+ 
+    gcd = 0
+    for i in range(n):
+        gcd = __gcd(gcd, a[i])
+ 
+    cnt = 0
+ 
+    for i in range(1, gcd + 1):
+        if i * i > gcd:
+            break
+        if (gcd % i == 0):
+ 
+            if (i * i == gcd):
+                cnt += 1
+ 
+            else:
+                cnt += 2
+ 
+    return cnt
+ 
+T=int(input())
+a=list(map(int,input().split()))
+
+
+ 
+print(getCount(a, T))
