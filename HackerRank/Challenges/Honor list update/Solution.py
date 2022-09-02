03006eaf97c07a5a0975485e9dfae185fc51a573
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2022-09-02 14:50:52 +0300

    Update Solution.py

--- HackerRank/Challenges/Honor list update/Solution.py
+++ HackerRank/Challenges/Honor list update/Solution.py
@@ -1,12 +1,34 @@
-test=int(input())
-for i in range(test):
-    N,M,g,y=input().split(" ")
-    total=int(N)*int(M)
-    mod=total-int(g)
-    if mod>=0:
-        if int(y)==0:
-            print(-1)
-        else:
-            print(mod)
+T = int(input())
+for i in range(T):
+    flag  = 0
+    flag2 = 0
+    C = int(input())
+    if C < 4:
+        flag = 1
+    if C < 3:
+        flag2 = 1
+    hours = 0
+    grades = 0
+    for c in range(C):
+        course = input()
+        course = course.split(" ")
+        if int(course[1]) < 80:
+            flag  = 1
+            flag2 = 1
+        hours += int(course[0])
+        grades += int(course[1]) * int(course[0])
+    average = grades / hours
+    if average < 85:
+        flag  =  1
+        flag2 =  1
+    if hours < 15:
+        flag = 1
+    if hours < 12:
+        flag2 = 1
+    if not flag:
+        print("Honor list")
     else:
-        print(0)
+        if not flag2:
+            print("Half honor list")
+        else:
+             print("Try next semester")
